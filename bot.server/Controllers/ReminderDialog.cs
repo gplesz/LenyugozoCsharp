6b5dc98db1c342b72e99eac1cfed94ce091848d2
Plesz Gábor <gabor.plesz@gmail.com>
2018-08-06 18:38:08 +0200

    Emlékeztető váza elkészült

---
+++ bot.server/Controllers/ReminderDialog.cs
@@ -0,0 +1,47 @@
+using System;
+using System.Threading.Tasks;
+using Microsoft.Bot.Builder.Dialogs;
+using Microsoft.Bot.Connector;
+
+namespace bot.server.Controllers
+{
+    [Serializable]
+    internal class ReminderDialog : IDialog<object>
+    {
+        const string ReminderTextKey = "Text";
+        public Task StartAsync(IDialogContext context)
+        {
+            context.PostAsync("Mire szeretnel emlekezni?").GetAwaiter().GetResult();
+
+            context.Wait(OnMessageReceived);
+
+            return Task.CompletedTask;
+        }
+
+        private Task OnMessageReceived(IDialogContext context, IAwaitable<object> result)
+        {
+            var message = (Activity)result.GetAwaiter().GetResult();
+
+            context.ConversationData.SetValue(ReminderTextKey, message.Text);
+
+            PromptDialog.Number(context, OnSecondReceived,
+             "Hany masodperc mulva szeretned kapni az ertesitot?");
+
+            return Task.CompletedTask;
+        }
+
+        private Task OnSecondReceived(IDialogContext context, IAwaitable<long> result)
+        {
+            var seconds = result.GetAwaiter().GetResult();
+            var text = context.ConversationData.GetValue<string>(ReminderTextKey);
+
+            context.PostAsync($"Ok, {seconds} masodperc mulva kuldunk ertesitot ezzel a szoveggel: {text} ")
+                .GetAwaiter()
+                .GetResult();
+
+            context.Done(true);
+            return Task.CompletedTask;
+
+        }
+    }
+}
---
+++ bot.server/Controllers/RootDialog.cs
@@ -0,0 +1,56 @@
+using System;
+using System.Threading.Tasks;
+using Microsoft.Bot.Builder.Dialogs;
+using Microsoft.Bot.Connector;
+
+namespace bot.server.Controllers
+{
+    [Serializable]
+    internal class RootDialog : IDialog<object>
+    {
+        public Task StartAsync(IDialogContext context)
+        {
+            context.Wait(OnMessageRecieved);
+
+            return Task.CompletedTask;
+        }
+
+        private Task OnMessageRecieved(IDialogContext context, IAwaitable<object> result)
+        {
+            var message = (Activity)result.GetAwaiter().GetResult();
+
+            context.PostAsync(message.CreateReply($"Ezt üzented:{ message.Text}"));
+
+            PromptDialog.Choice(context, OnChoiceRecieved,
+                new[] { "Emlekezteto", "Kepfelismero", "Arcazonositas", "Idojaras" },
+                "Menupontok:");
+
+            return Task.CompletedTask;
+        }
+
+        private Task OnChoiceRecieved(IDialogContext context, IAwaitable<string> result)
+        {
+            var choice = result.GetAwaiter().GetResult();
+
+            switch (choice)
+            {
+                case "Emlekezteto":
+                    context.Call(new ReminderDialog(), OnDialogDone);
+                    break;
+                default:
+                    context.PostAsync($"Hmm, erre meg nem vagyok felkeszitve, ezt valasztottad: {choice}")
+                        .GetAwaiter()
+                        .GetResult();
+                    break;
+            }
+
+            return Task.CompletedTask;
+        }
+
+        private Task OnDialogDone(IDialogContext context, IAwaitable<object> result)
+        {
+
+            return Task.CompletedTask;
+        }
+    }
+}
